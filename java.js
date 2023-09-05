b44fd93790ef7b85ebd53150a515dac342e3cf88
ei chanudom <131318701+domkh12@users.noreply.github.com>
2023-09-05 08:34:34 +0700

    Add files via upload

--- java.js
+++ java.js
@@ -3,8 +3,8 @@ y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
 day = n.getDay();
-dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-monthNames = ["January", "February", "March", "-", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+dayNames = ["អាទិត្យ", "ចន្ទ", "អង្គារ", "ពុធ", "ព្រហស្បតិ៍", "សុក្រ", "សៅរ៍"];
+monthNames = ["មករា", "កុម្ភៈ", "មីនា", "មេសា", "ឧសភា", "មិថុនា", "កក្កដា", "សីហា", "កញ្ញា", "តុលា", "វិច្ឆិកា", "ធ្នូ"];
 document.getElementById("date").innerHTML = dayNames[day] + "," + d + " " + monthNames[m] + " " + y;
 
 
@@ -29,4 +29,25 @@ loadmoreBtn.onclick = () => {
   if(currentitem >= boxes.length){
     loadmoreBtn.style.display = 'none';
   }
+}
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
